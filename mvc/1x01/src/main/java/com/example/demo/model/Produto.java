3006a6e10eb7d5cdea4e6f470d81a17bcf13b903
TAMAS PETER BATTONYAI JUNIOR <tamas.battonyai@bradesco.com.br>
2025-11-13 22:05:04 -0300

    Spring MVC – Crud com Rest

---
+++ mvc/1x01/src/main/java/com/example/demo/model/Produto.java
@@ -0,0 +1,53 @@
+package com.example.demo.model;
+
+public class Produto {
+    private Long id;
+    private String nome;
+    private double preco;
+
+    public Produto() {}
+
+    public Produto(Long id, String nome, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
+}
